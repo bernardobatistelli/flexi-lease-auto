cf2ea1234e625124285a28c76e161be0b54e72ee
Bernardo Batistelli <bebatistelli254@gmail.com>
2024-07-08 08:55:32 -0300

    feat : get car by id use case

--- src/use-cases/cars/create-car.ts
+++ src/use-cases/cars/create-car.ts
@@ -3,6 +3,7 @@ import { CreateCarDto } from '../../@types/DTOs/cars/create-car-dto'
 import { ICar } from '../../@types/interfaces/car-interface'
 import { CarsRepository } from '../../repositories/cars-repository'
 import { InvalidYearError } from '../errors/invalid-year'
+import { SameAccessoryError } from '../errors/same-accessory'
 
 export class CreateCarUseCase {
   constructor(private carRepository: CarsRepository) {}
@@ -10,6 +11,19 @@ export class CreateCarUseCase {
   async execute(data: CreateCarDto): Promise<ICar> {
     if (parseInt(data.year) <= 1950 || parseInt(data.year) >= 2023) {
       throw new InvalidYearError()
+    }
+
+    if (
+      data.accessories.some((accessory, index) => {
+        return data.accessories
+          .slice(index + 1)
+          .some(
+            (otherAccessory) =>
+              accessory.description === otherAccessory.description,
+          )
+      })
+    ) {
+      throw new SameAccessoryError()
     }
 
     const car = await this.carRepository.create(data)
